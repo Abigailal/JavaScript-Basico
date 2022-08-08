55624cca897a6e3fe7cc2d3fd429ad09f29f9aa1
Abigailal <abigail_al@outlook.com>
2022-08-08 13:24:42 -0500

    styles

--- JS/estilos.js
+++ JS/estilos.js
@@ -1,10 +1,10 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".nav-menu");
+const navToggle = document.querySelector(".nav__toggle");
+const navMenu = document.querySelector(".nav__menu");
 
 navToggle.addEventListener("click", () => {
-  navMenu.classList.toggle("nav-menu_visible");
+  navMenu.classList.toggle("nav__menu--visible");
 
-  if (navMenu.classList.contains("nav-menu_visible")) {
+  if (navMenu.classList.contains("nav-menu--visible")) {
     navToggle.setAttribute("aria-label", "Cerrar menú");
   } else {
     navToggle.setAttribute("aria-label", "Abrir menú");
